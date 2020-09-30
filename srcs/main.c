aecee1983c63dd7345abcfa07d898a496f7b4919
Emile BEDOISEAU <ebedoise@e3r4p18.42.fr>
2020-09-30 14:29:51 +0200

    n params

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: iouali <iouali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 18:32:43 by iouali            #+#    #+#             */
-/*   Updated: 2020/09/30 14:14:23 by ebedoise         ###   ########.fr       */
+/*   Updated: 2020/09/30 14:28:37 by ebedoise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,22 @@ int		main(int ac, char **av)
 	while (i < ac)
 	{
 		matrix = 0;
-		datas = parsing(av[1]);
+		datas = parsing(av[i]);
 		if (datas.matrix == 0)
-		{
 			write(1, "map error\n", 10);
-			return (0);
-		}
-		datas = solver(datas);
-		if (datas.x == 0 && datas.y == 0 && datas.best_size == 0 &&
-				datas.matrix[0][0] == datas.obstacle)
+		else
 		{
-			write(1, "map error\n", 10);
-			return (0);
+			datas = solver(datas);
+			if (datas.x == 0 && datas.y == 0 && datas.best_size == 0 &&
+					datas.matrix[0][0] == datas.obstacle)
+				write(1, "map error\n", 10);
+			else
+			{
+				matrix = fill_soluce(datas, matrix);
+				print_matrix(datas, matrix);
+			}
 		}
-		matrix = fill_soluce(datas, matrix);
-		print_matrix(datas, matrix);
+		write(1, "\n", 1);
 		i++;
 	}
 	return (0);
