2e0236849269f6f2733f30e2eab419538d217969
Ismael OUALI <iouali@e3r2p20.42.fr>
2020-10-01 10:19:40 +0200

    correction pas de soluce si stdin

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: iouali <iouali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 18:32:43 by iouali            #+#    #+#             */
-/*   Updated: 2020/10/01 10:01:23 by iouali           ###   ########.fr       */
+/*   Updated: 2020/10/01 10:18:04 by iouali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
-int		no_params(void)
+int		no_params(int ac)
 {
 	t_plat_info	datas;
 	char		**matrix;
@@ -27,7 +27,7 @@ int		no_params(void)
 		datas = solver(datas);
 		if (datas.x == 0 && datas.y == 0 && datas.best_size == 0 &&
 				datas.matrix[0][0] == datas.obstacle)
-			write(1, "map error\n", 10);
+			print_matrix(datas, datas.matrix, ac, 0);
 		else
 			print_matrix(datas, fill_soluce(datas, matrix, 0), 0, 0);
 	}
@@ -40,14 +40,14 @@ int		main(int ac, char **av)
 	char		**matrix;
 	int			i;
 
-	ac == 1 ? no_params() : 0;
+	ac == 1 ? no_params(ac) : 0;
 	i = 1;
 	while (i < ac)
 	{
 		matrix = 0;
 		datas.matrix = 0;
 		datas = parsing(av[i]);
-		if (datas.matrix == 0)
+		if (datas.matrix)
 			i < ac - 1 ? ft_str("map error\n\n") : ft_str("map error\n");
 		else
 		{
